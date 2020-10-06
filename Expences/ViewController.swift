85bcd2bf57c967a44d8783d8ff6d7756a7f79b88
Serhii Prykhodko <serhii.prikhodko@gmail.com>
2020-10-06 15:43:09 +0300

    Initial commit

---
+++ Expences/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expences
+//
+//  Created by Yaroslav Luchyt on 06.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
