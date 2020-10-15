cf7be493297815a931f785ca17cfd026b69dff4c
Serhii Prikhodko <46839542+serhii-prikhodko@users.noreply.github.com>
2020-10-15 14:46:37 +0300

    UI tests (#2)
    * - added basic UI tests
    
    * Fixed tests behaviour

--- expences-app-swiftuiUITests/expences_app_swiftuiUITests.swift
+++ expences-app-swiftuiUITests/expences_app_swiftuiUITests.swift
@@ -8,29 +8,70 @@
 import XCTest
 
 class expences_app_swiftuiUITests: XCTestCase {
-
+    
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    override func setUp() {
+        self.app = XCUIApplication()
+        self.app.launch()
     }
-
+    
+    func testButtonTextOnLaunchScreen() throws {
+        // Find button by name
+        let button = app.buttons["Show Expenses"]
+        
+        XCTContext.runActivity(named: "Open launch screen") { _ in
+            XCTAssertTrue(button.waitForExistence(timeout: 5.0))
+            
+            // Check that button is presented on the screen
+            XCTAssertTrue(self.app.buttons["Show Expenses"].exists, "Button 'Show Expenses' isn't presented on the screen")
+        }
+    }
+    
+    func testGoToExpensesScreen() throws {
+        // Find button by name
+        let button = app.buttons["Show Expenses"]
+        
+        XCTContext.runActivity(named: "Open launch screen") { _ in
+            XCTAssertTrue(button.waitForExistence(timeout: 5.0))
+            
+            // Open expenses screen
+            button.tap()
+            
+            // Check screen navigation title text
+            XCTAssertTrue(app.navigationBars.staticTexts["Expenses"].waitForExistence(timeout: 5.0), "Can't find text 'Expenses' on the navigation bar")
+        }
+    }
+    
+    func testCheckExpensesData() throws {
+        // Find button by name
+        let button = app.buttons["Show Expenses"]
+        
+        XCTContext.runActivity(named: "Open launch screen") { _ in
+            XCTAssertTrue(button.waitForExistence(timeout: 5.0))
+            
+            // Open expenses screen
+            button.tap()
+            
+            // Check first expense title and price
+            XCTAssertTrue(app.cells.staticTexts["Beer"].waitForExistence(timeout: 5.0), "Can't find data 'Beer' on the screen")
+            XCTAssertTrue(app.cells.staticTexts["$ 19.00"].waitForExistence(timeout: 5.0), "Can't find data '19.00' on the screen")
+        }
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
